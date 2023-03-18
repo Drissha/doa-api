5c74615d594e82eeb297a578cc58b1499498cae1
drissha <rian.alfazri96@gmail.com>
2023-03-18 22:05:04 +0700

    Get Beres

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,34 @@
+$.ajax({
+    url: `https://64140ebbebce1f9d8c5df836.mockapi.io/api/v2/kumpulandoa`,
+    type: 'get',
+    dataType: 'json',
+    data:{
+        doa : `doa`
+    },
+    success: function(data) {
+        let counter = 0; 
+        $.each(data, function(index, item) {
+          if (counter < 5) { 
+            $('#doa-info').append(`
+            <button class="btn btn-primary mx-2 w-auto detail" type="button" 
+            data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="${item['id']}">
+                ${item['doa']}
+            </button>
+            `);
+            // <h5 class="card-title">${item['doa']}</h5>
+            counter++; 
+          } else {
+            return false; 
+          }
+        });
+
+    },
+    
+  });
+
+
 function showCard(params) {
-    $('#card-info').html('');
+    $('#doa-info').html('');
 
     const searchQuery = $('#search-input').val();
 
@@ -11,12 +40,15 @@ function showCard(params) {
             'doa': `${searchQuery}`
         },
         success: function(data) {
-            console.log(data);
+            // console.log(data);
   
             if (data.length > 1) {
                 $.each(data, function(key, item) {
-                    $('#card-info').append(`
-                        <h5 class="card-title">${item['doa']}</h5>
+                    $('#doa-info').append(`
+                    <button class="btn btn-primary mx-2 w-auto detail" type="button" 
+                    data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="${item['id']}">
+                        ${item['doa']}
+                    </button>
                     `);
                 });
             } else {
@@ -35,4 +67,32 @@ $('#search-input').on('keyup', function (e) {
     if (e.which === 13) {
         showCard();
     }
+})
+
+$('#doa-info').on('click', '.detail', function () {
+    const id = $(this).data('id');
+    // console.log($this.data('id'));
+    $.ajax({
+        url: `https://64140ebbebce1f9d8c5df836.mockapi.io/api/v2/kumpulandoa/${id}`,
+        type: 'get',
+        dataType: 'json',
+        data:{
+            'id': id
+        },
+        success: function(data) {
+            $('.modal-header').html(`
+            <h1 class="modal-title fs-5" id="exampleModalLabel">${data.doa}</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            `)
+          $('.modal-body').html(`
+          <p>${data.ayat}</p>
+          <p>${data.latin}</p>
+          <p>${data.artinya}</p>
+          `)
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          console.error(errorThrown);
+          alert('tidak ditemukan');
+        }
+      });
 })
